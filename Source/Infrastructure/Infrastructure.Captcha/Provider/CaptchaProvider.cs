b86043430c2fc448bcbd392e37954aeeec183c62
Irina Andriamifidy <nalijaona.andriamifidy@proximitybbdo.fr>
2019-09-14 22:15:41 +0400

    Implemented Captcha validation infrastructure

---
+++ Source/Infrastructure/Infrastructure.Captcha/Provider/CaptchaProvider.cs
@@ -0,0 +1,21 @@
+﻿using Core.Infrastructure.Interfaces.Validator;
+using Infrastructure.Captcha.Helper;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Captcha.Provider
+{
+    public class CaptchaProvider : IFormValidatorProvider
+    {
+        private readonly IConfiguration _config;
+
+        public CaptchaProvider(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public bool Validate(string response)
+        {
+            return GoogleRecaptchaHelper.ValidateReCaptchaV2(_config, response);
+        }
+    }
+}
